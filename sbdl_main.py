5365d125eaa8a8874a30239b1557a5dbeadcbb3c
Ganesh Kattekola <ganesh.kattekola@ibm.com>
2023-02-27 12:27:02 +0530

    deleted few comments

--- sbdl_main.py
+++ sbdl_main.py
@@ -16,9 +16,6 @@ if __name__ == '__main__':
     logger = Log4j(spark)
 
     logger.info("Finished creating Spark Session")
-    This is Kattekola
-    This is Ganesh
-    Wife is Tejasvi
-    Daugthers are Harshini & Tanvi
+     Daugthers are Harshini & Tanvi
 
     
